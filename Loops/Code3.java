ef9b41bb9ad23ee62583894dabcfc057c4f028ff
Raghavendra d g <145965115+raghu1827@users.noreply.github.com>
2025-01-29 11:28:32 +0530

    Add files via upload

---
+++ Loops/Code3.java
@@ -0,0 +1,17 @@
+package Loops;
+
+//Print numbers from 10 to 1 in reverse order using a for loop.
+
+public class Code3 {
+
+    public static void main(String[] args) {
+
+        // Declare variable
+        int num = 10;
+
+        // Itearate over in reverse order
+        for (int i = num; i >= 1; i--) {
+            System.out.println("i:" + i);
+        }
+    }
+}
