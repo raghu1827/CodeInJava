ee8821c3da0c321e10b6d0383a35d07830407734
Raghavendra d g <145965115+raghu1827@users.noreply.github.com>
2025-02-11 12:46:08 +0530

    Add files via upload

---
+++ Patterns/Number/Pattern4.java
@@ -0,0 +1,29 @@
+package Patterns.Number;
+
+/*
+Pattern
+
+7 7 7 7 7 7 7
+6 6 6 6 6 6
+5 5 5 5 5
+4 4 4 4
+3 3 3
+2 2
+1
+
+*/
+
+public class Pattern4 {
+    public static void main(String[] args) {
+
+        int n = 7;
+
+        // Pattern Logic
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
