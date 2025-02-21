acfcf4f13ee01206e5d1558f22f3cc30edf2e637
Raghavendra d g <145965115+raghu1827@users.noreply.github.com>
2025-02-21 19:57:44 +0530

    Update Pattern20.java

--- Patterns/Pattern20.java
+++ Patterns/Pattern20.java
@@ -1,5 +1,16 @@
 package Patterns;
 
+/*
+
+ ***********
+  *       * 
+   *     *  
+    *   *   
+     * *    
+      * 
+
+*/
+
 public class Pattern20 {
 
     public static void main(String[] args) {
